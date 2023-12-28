5ef0245f6e0716f58582edb88916c58dcc95582d
sopiko abulidze <abulidze2019@gmail.com>
2023-12-28 14:58:10 +0000

    connect database to code and heroku, ready for first heroku deployment

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--dx5t7i)@-3-hnie-fm5isge^oj4_iy4cb!_=^8=h%(p@h24b_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [ '8000-tamariam-restauranttama-644kgenlgv6.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = [ '8000-tamariam-restauranttama-644kgenlgv6.ws-eu107.gitpod.io','.herokuapp.com']
 
 
 # Application definition
@@ -74,13 +78,17 @@ WSGI_APPLICATION = 'restaurant.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
