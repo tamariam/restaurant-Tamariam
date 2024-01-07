89aa3871f9bc4954ae5ccdd1a2d961180d157ff2
sopiko abulidze <abulidze2019@gmail.com>
2024-01-07 21:14:29 +0000

    add comments to base html and style.css

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'SECRET_KEY = os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-tamariam-restauranttama-644kgenlgv6.ws-eu107.gitpod.io', '.herokuapp.com']
 
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
