42225ab798d0211d564cbbc09259dde51ac1013f
sopiko abulidze <abulidze2019@gmail.com>
2024-01-17 14:49:22 +0000

    make changes to email settings, change variable name

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -188,9 +188,9 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_USER')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL__HOST_USER')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
