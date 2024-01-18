49f53ba0724e9397d0a97066682f54a6d86159f0
sopiko abulidze <abulidze2019@gmail.com>
2024-01-18 21:17:30 +0000

    doble check sign up,login update_profile ,delete-account files and give appropriate styles to its buttons and stcucture

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -179,12 +179,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # email settings Configuration
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp-mail.outlook.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_HOST = "smtp-mail.outlook.com"
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 
 
 
