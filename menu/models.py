cfeb972076ea77b85768bc8e43b3516e2f2d4b92
sopiko abulidze <abulidze2019@gmail.com>
2024-01-25 13:33:39 +0000

    Add comments to menu model, fix spelling error in availability field

--- menu/models.py
+++ menu/models.py
@@ -3,7 +3,17 @@ from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
+
+# Define choices for menu courses
+
 MENU_COURSES = (("mtarter", "starter"), ("main", "main"), ("dessert", "dessert"))
+
+# Define choices for status
+
+''' # Model representing a menu item with its name, ingredients, price,
+ availability, status, category, and featured image'''
+
+
 STATUS = ((0, "Draft"), (1, "Published"))
 
 
@@ -11,9 +21,10 @@ class Menu(models.Model):
     name = models.CharField(max_length=100, unique=True)
     ingredients = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2, default='00.00')
-    avability = models.BooleanField(default=True)
+    availability = models.BooleanField(default=True)
     status = models.IntegerField(choices=STATUS, default=0)
     category = models.CharField(choices=MENU_COURSES, default="Main", max_length=20)
     featured_image = CloudinaryField('image', default='placeholder')
+
     def __str__(self):
         return self.name
