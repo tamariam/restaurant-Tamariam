5638d6b182b191d4a0e917a5f4863de140a64081
sopiko abulidze <abulidze2019@gmail.com>
2024-01-28 20:59:44 +0000

    add comments to menu .html and views.py files

--- menu/views.py
+++ menu/views.py
@@ -4,15 +4,21 @@ from .models import Menu
 
 # Create your views here.
 
+'''View function for rendering the menu page.'''
+
+
 def menu_page(request):
+    # Retrieve all menu items categorized as starters, mains, and desserts
     starters = Menu.objects.filter(status=1, category='starter')
     mains = Menu.objects.filter(status=1, category='main')
-    desserts = Menu.objects.filter(status=1, category='dessert')   
+    desserts = Menu.objects.filter(status=1, category='dessert')
 
+    # Prepare context data to pass to the template
     context = {
         'starters': starters,
         'mains': mains,
         'desserts': desserts,
-     }
+    }
 
+    # Render the menu page template with the provided context data
     return render(request, "menu/menu.html", context)
