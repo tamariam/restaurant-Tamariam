a19b55e86aafef909f5418b09ceaba44c71037c4
sopiko abulidze <abulidze2019@gmail.com>
2024-02-05 22:07:09 +0000

    add dislay messages function  verification_sent.html to display succes message when verification is sent

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -28,10 +28,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'SECRET_KEY = os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-tamariam-restauranttama-644kgenlgv6.ws-eu107.gitpod.io',
+    '8000-tamariam-restauranttama-644kgenlgv6.ws-eu108.gitpod.io',
     '.herokuapp.com'
 ]
 
