fec955fb2faf18e287686a8e7c13f0a4c824b4d2
sopiko abulidze <abulidze2019@gmail.com>
2024-02-08 15:46:37 +0000

    create   view to create booking

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import BookingForm
+from django.contrib import messages
 
 
 # Create your views here.
 
 def booking_page(request):
-    return render(request, 'booking/booking.html')
+    user = request.user
+    if request.method == "POST":
+        form = BookingForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'Profile created succesfully')
+            return redirect('profile_page')
+        else:
+            messages.warning(request, "Booking failed. Please correct the errors below.")
+    else:
+        form = BookingForm(instance=user)
+    return render(request, "booking/booking.html", {'form': form})
+
