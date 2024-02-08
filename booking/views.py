328a84119905fd6c592696d03445f1e0f91b033c
sopiko abulidze <abulidze2019@gmail.com>
2024-02-08 16:25:16 +0000

    i realize the view which i created was for creatuing bookings, firstly what i want is to display the form, so i create another view  and  write code in booking template to display this form

--- booking/views.py
+++ booking/views.py
@@ -1,21 +1,25 @@
 from django.shortcuts import render, redirect
 from .models import BookingForm
 from django.contrib import messages
+from .models import Booking
 
 
-# Create your views here.
+# Create your views here.Menu.objects.filter(status=1, category='starter')
+
+# def booking_page(request):
+#     if request.method == "POST":
+#         form = BookingForm(request.POST,)
+#         if form.is_valid():
+#             form.save()
+#             messages.add_message(request, messages.SUCCESS, 'Profile created succesfully')
+#             return redirect('profile_page')
+#         else:
+#             messages.warning(request, "Booking failed. Please correct the errors below.")
+#     else:
+#         form = BookingForm()
+#     return render(request, "booking/booking.html", {'form': form})
 
 def booking_page(request):
-    user = request.user
-    if request.method == "POST":
-        form = BookingForm(request.POST, instance=user)
-        if form.is_valid():
-            form.save()
-            messages.add_message(request, messages.SUCCESS, 'Profile created succesfully')
-            return redirect('profile_page')
-        else:
-            messages.warning(request, "Booking failed. Please correct the errors below.")
-    else:
-        form = BookingForm(instance=user)
+    form = BookingForm()
     return render(request, "booking/booking.html", {'form': form})
 
