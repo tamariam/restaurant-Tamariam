da6c02bf741d9f75ab1c462ce43f83dcb54231d7
sopiko abulidze <abulidze2019@gmail.com>
2024-02-10 13:34:27 +0000

    the default for the 'time' field in the Booking model was set to an integer, causing migration issues. I Revised the TIME tuple to use string representations of time values to address the migration problem, then Discovered a better solution by importing 'time' from the datetime module and using time objects in the TIME tuple and the 'time' field.This refactor worsk as expected

--- booking/models.py
+++ booking/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # from django.forms import ModelForm
 # Create your models here.
+from datetime import time
+
 
 STATUS = (
     (0, "Pending"), (1, "Approved",), (2, "Rejected")
@@ -16,19 +18,26 @@ PEOPLE = ((1, "1 person"),
           (6, "6 person"),
           (7, "7 person"))
 
-TIME = ((0, "18:00"), (1, "19:00"), (2, "20:00"), (3, "21:00"),  (4, "22:00"), (5, "23:00"))
+TIME = (
+    (time(18,0), "18:00"),
+    (time(19,0), "19:00"), 
+    (time(20,0), "20:00"),
+    (time(21,0), "21:00"),
+    (time(22,0), "22:00"),
+    (time(23,0), "23:00")
+   )
 
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=100)
-    last_name = models. CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     status = models.IntegerField(choices=STATUS, default=0)
     num_of_people = models.IntegerField(choices=PEOPLE, default=1)
     request_date = models.DateTimeField(auto_now_add=True)
     date = models.DateField()
-    time = models.TimeField(choices=TIME, default=0)
+    time = models.TimeField(choices=TIME, default=time(18, 0))
 
     def __str__(self):
         return self.first_name
