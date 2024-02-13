df9d31534d432f5fec74e4c653b38031a5f998ba
sopiko abulidze <abulidze2019@gmail.com>
2024-02-13 19:27:14 +0000

    Previously, when a user selected a past date in the booking form, an error message was displayed. However, upon reviewing the code, I realized that it would be more user-friendly to prevent users from selecting past dates altogether.So, I updated the date field widget in the BookingForm class to include the 'min' parameter set to today's date

--- booking/forms.py
+++ booking/forms.py
@@ -1,11 +1,11 @@
 from .models import Booking
 from django import forms
-
+from django.utils import timezone
 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ["date", "time", "num_of_people"]
         widgets = {
-            'date': forms.DateInput(attrs={'type': 'date'}),
+            'date': forms.DateInput(attrs={'type': 'date', 'min': timezone.now().date()})
         }
