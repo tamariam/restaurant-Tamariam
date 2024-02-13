51800455bb0670cd18608c785c22de78b2ff1aae
sopiko abulidze <abulidze2019@gmail.com>
2024-02-13 23:21:15 +0000

    i modified booking_page view to handle two situations: 1.If the maximum number of accepted bookings for a particular date has been reached, users will not be able to make additional bookings for that date. An error message is displayed to inform the user.  2.If a user tries to book a table for a number of people that exceeds the maximum capacity, the booking will be rejected. The view now checks the total number of people already booked for the specified date and compares it against the maximum capacity to determine if the booking is allowed

--- booking/models.py
+++ booking/models.py
@@ -7,7 +7,7 @@ from datetime import time
 
 
 STATUS = (
-    (0, "Pending"), (1, "Approved",), (2, "Rejected")
+    (0, "Pending"), (1, "Approved"), (2, "Rejected")
     )
 
 PEOPLE = ((1, "1 person"),
@@ -19,12 +19,12 @@ PEOPLE = ((1, "1 person"),
           (7, "7 person"))
 
 TIME = (
-    (time(18,0), "18:00"),
-    (time(19,0), "19:00"), 
-    (time(20,0), "20:00"),
-    (time(21,0), "21:00"),
-    (time(22,0), "22:00"),
-    (time(23,0), "23:00")
+    (time(18, 0), "18:00"),
+    (time(19, 0), "19:00"), 
+    (time(20, 0), "20:00"),
+    (time(21, 0), "21:00"),
+    (time(22, 0), "22:00"),
+    (time(23, 0), "23:00")
    )
 
 
@@ -34,7 +34,7 @@ class Booking(models.Model):
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
     status = models.IntegerField(choices=STATUS, default=0)
-    num_of_people = models.IntegerField(choices=PEOPLE, default=1)
+    num_of_people = models.IntegerField(choices=PEOPLE, default=0)
     request_date = models.DateTimeField(auto_now_add=True)
     date = models.DateField()
     time = models.TimeField(choices=TIME, default=time(18, 0))
--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import BookingForm
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from . models import Booking
-from django.utils import timezone
+
 
 
 
@@ -31,30 +30,63 @@ from django.utils import timezone
 #         messages.add_message(request, messages.WARNING, 'PLEASE SIGN UP OR LIGIN FIRST TO MAKE  BOOKING')
 #         return redirect('account_login')
 
+# def booking_page(request):
+#     if request.user.is_authenticated:
+#         form = BookingForm(data=request.POST) if request.method == "POST" else BookingForm()
+#         if request.method == "POST":
+#             if form.is_valid():
+#                 booking = form.save(commit=False)
+#                 booking.email = request.user.email
+#                 booking.user = request.user
+#                 booking.first_name = request.user.first_name
+#                 booking.last_name = request.user.last_name
+#                 booked = Booking.objects.filter(date=booking.date, time=booking.time, user=request.user)
+#                 if booked:
+#                     messages.add_message(request, messages.SUCCESS, 'You are already booked for this date and time please select another time ')
+#                 else:
+#                     booking.save()
+#                     messages.add_message(request, messages.SUCCESS, 'Your booking request has been sent')
+#                     return redirect('profile_page')
+#             else:
+#                 messages.warning(request, "Booking failed. Please correct the errors below.")
+#         return render(request, "booking/booking.html", {'form': form})
+#     else:
+#         messages.add_message(request, messages.WARNING, 'PLEASE SIGN UP OR LOGIN FIRST TO MAKE  BOOKING')
+#         return redirect('account_login')
+
+
 def booking_page(request):
     if request.user.is_authenticated:
         form = BookingForm(data=request.POST) if request.method == "POST" else BookingForm()
         if request.method == "POST":
             if form.is_valid():
                 booking = form.save(commit=False)
-                booking.email = request.user.email
-                booking.user = request.user
-                booking.first_name = request.user.first_name
-                booking.last_name = request.user.last_name
-                booked = Booking.objects.filter(date=booking.date, time=booking.time, user=request.user)
-                if booked:
-                    messages.add_message(request, messages.SUCCESS, 'You are already booked for this date and time please select another time ')
+                current_bookings = Booking.objects.filter(date=booking.date, status=1)
+                total_people_booked = sum(booking.num_of_people for booking in current_bookings)
+                requested_people = booking.num_of_people
+                max_capacity = 7
+                if total_people_booked >= max_capacity:
+                    messages.error(request, 'OOPS')
+                elif requested_people + total_people_booked > max_capacity:
+                    messages.error(request,  'nonononon')
                 else:
-                    booking.save()
-                    messages.add_message(request, messages.SUCCESS, 'Your booking request has been sent')
-                    return redirect('profile_page')
+                    booking.email = request.user.email
+                    booking.user = request.user
+                    booking.first_name = request.user.first_name
+                    booking.last_name = request.user.last_name
+                    booked = Booking.objects.filter(date=booking.date, time=booking.time, user=request.user)
+                    if booked:
+                        messages.add_message(request, messages.SUCCESS, 'You are already booked for this date and time please select another time ')
+                    else:
+                        booking.save()
+                        messages.add_message(request, messages.SUCCESS, 'Your booking request has been sent')
+                        return redirect('profile_page')
             else:
                 messages.warning(request, "Booking failed. Please correct the errors below.")
         return render(request, "booking/booking.html", {'form': form})
     else:
         messages.add_message(request, messages.WARNING, 'PLEASE SIGN UP OR LOGIN FIRST TO MAKE  BOOKING')
         return redirect('account_login')
-
 
     
 
