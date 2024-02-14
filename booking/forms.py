24f8232bca0b1de16b13735e01c21b49ed0fa3b6
sopiko abulidze <abulidze2019@gmail.com>
2024-02-14 11:57:51 +0000

    add docstrings to bookingform class

--- booking/forms.py
+++ booking/forms.py
@@ -2,7 +2,14 @@ from .models import Booking
 from django import forms
 from django.utils import timezone
 
+
 class BookingForm(forms.ModelForm):
+    """
+    A form for creating or updating a booking.
+    This form allows users to input information such as the date, time,
+    and number of people for a booking. It utilizes Django's ModelForm
+    functionality to directly map fields to a Booking model."""
+
     class Meta:
         model = Booking
         fields = ["date", "time", "num_of_people"]
