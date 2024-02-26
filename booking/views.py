3ae5535341102957073748ac71e4ea197654e031
sopiko abulidze <abulidze2019@gmail.com>
2024-02-26 13:44:18 +0000

    Add else block for staff members in booking page view to restrict rendering permission

--- booking/views.py
+++ booking/views.py
@@ -10,70 +10,75 @@ from django.shortcuts import get_object_or_404
 
 def booking_page(request):
     if request.user.is_authenticated:
-        # Initialize form with POST data if available, otherwise empty form
-        form = BookingForm(
-            data=request.POST) if request.method == "POST" else BookingForm()
-        if request.method == "POST":
-            if form.is_valid():
-                # Create booking object but do not save to database yet
-                booking = form.save(commit=False)
-                # Check current bookings for the selected date
-                current_bookings = Booking.objects.filter(
-                    date=booking.date, status="approved")
-                # Calculate total people already booked for the selected date
-                total_people_booked = sum(
-                    booking.num_of_people for booking in current_bookings)
-                # Get number of people requested for the booking
-                requested_people = booking.num_of_people
-                # Define max capacity for bookings
-                max_capacity = 7
-                # Check if booking exceeds maximum capacity or clashes with
-                # existing bookings
-                if total_people_booked >= max_capacity:
-                    messages.error(
-                        request,
-                        'We are currently fully booked '
-                        'for this date.Please choose another date or time.'
-                    )
-                elif requested_people + total_people_booked > max_capacity:
-                    messages.error(
-                        request,
-                        'Unfortunately, your booking request exceeds the '
-                        'maximum capacity for this date. Please select fewer '
-                        'people or choose another date or time.'
+        if not request.user.is_staff:
+            # Initialize form with POST data if available, otherwise empty form
+            form = BookingForm(
+                data=request.POST) if request.method == "POST" else BookingForm()
+            if request.method == "POST":
+                if form.is_valid():
+                    # Create booking object but do not save to database yet
+                    booking = form.save(commit=False)
+                    # Check current bookings for the selected date
+                    current_bookings = Booking.objects.filter(
+                        date=booking.date, status="approved")
+                    # Calculate total people already booked for the selected date
+                    total_people_booked = sum(
+                        booking.num_of_people for booking in current_bookings)
+                    # Get number of people requested for the booking
+                    requested_people = booking.num_of_people
+                    # Define max capacity for bookings
+                    max_capacity = 7
+                    # Check if booking exceeds maximum capacity or clashes with
+                    # existing bookings
+                    if total_people_booked >= max_capacity:
+                        messages.error(
+                            request,
+                            'We are currently fully booked '
+                            'for this date.Please choose another date or time.'
                         )
-                else:
-                    # Update booking details and save to database
-                    booking.email = request.user.email
-                    booking.user = request.user
-                    booking.first_name = request.user.first_name
-                    booking.last_name = request.user.last_name
-                    # Check if user has already booked for the same date and
-                    # time
-                    booked = Booking.objects.filter(
-                        date=booking.date, time=booking.time, user=request.user
-                        )
-                    if booked:
-                        messages.add_message(
-                            request, messages.ERROR,
-                            'You already have a booking for this date and '
-                            'time. Please choose a different time slot.'
+                    elif requested_people + total_people_booked > max_capacity:
+                        messages.error(
+                            request,
+                            'Unfortunately, your booking request exceeds the '
+                            'maximum capacity for this date. Please select fewer '
+                            'people or choose another date or time.'
                             )
                     else:
-                        booking.save()
-                        messages.add_message(
-                            request,
-                            messages.SUCCESS,
-                            'Your booking request has been successfully '
-                            'submitted!You will receive feedback from us '
-                            'within the next 24 hours regarding the status '
-                            'of your booking.'
+                        # Update booking details and save to database
+                        booking.email = request.user.email
+                        booking.user = request.user
+                        booking.first_name = request.user.first_name
+                        booking.last_name = request.user.last_name
+                        # Check if user has already booked for the same date and
+                        # time
+                        booked = Booking.objects.filter(
+                            date=booking.date, time=booking.time, user=request.user
                             )
-                        return redirect('profile_page')
-            else:
-                messages.error(
-                    request, "Booking failed. Please correct the errors below")
-        return render(request, "booking/booking.html", {'form': form})
+                        if booked:
+                            messages.add_message(
+                                request, messages.ERROR,
+                                'You already have a booking for this date and '
+                                'time. Please choose a different time slot.'
+                                )
+                        else:
+                            booking.save()
+                            messages.add_message(
+                                request,
+                                messages.SUCCESS,
+                                'Your booking request has been successfully '
+                                'submitted!You will receive feedback from us '
+                                'within the next 24 hours regarding the status '
+                                'of your booking.'
+                                )
+                            return redirect('profile_page')
+                else:
+                    messages.error(
+                        request, "Booking failed. Please correct the errors below")
+            return render(request, "booking/booking.html", {'form': form})
+        else:
+            messages.error(
+                        request, "as staff member You are not authorised to   make booking")
+            return redirect('home')
     else:
         messages.add_message(
             request,
