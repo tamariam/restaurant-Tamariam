1452385ca74566a4b5209b38057f0b4cc1ba4654
sopiko abulidze <abulidze2019@gmail.com>
2024-02-26 16:59:53 +0000

    update  dashboard_page view, and dashboard html file to display  bookings in three sections pendings approves and rejected bookings,i use template logic to dislay correct bookings

--- staff/views.py
+++ staff/views.py
@@ -5,7 +5,9 @@ from booking.models import Booking
 
 
 def dashboard_page(request):
-    # return render('staff/dahsboard.html', {'dashboard_url': 'dashboard'})
-
-    return render(request, "staff/dashboard.html", {'dashboard_url': 'dashboard'})
+    bookings = Booking.objects.all()
+    pendings = Booking.objects.filter(status='pending')
+    approved = Booking.objects.filter(status='approved')
+    rejected = Booking.objects.filter(status = 'rejected')
+    return render(request, "staff/dashboard.html", {'dashboard_url': 'dashboard','bookings':bookings, 'pendings': pendings, 'approved': approved, 'rejected': rejected})
 
