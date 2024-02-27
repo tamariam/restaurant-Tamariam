6262db6ea0b83c9e75d5cb961d8b0a44a27e7572
sopiko abulidze <abulidze2019@gmail.com>
2024-02-27 14:22:22 +0000

    Create  approve' view to handle the approval process for staff members. This view updates the status of bookings to approved when a POST request is received. Also modified the template to include a form button within the table cell, allowing staff members to approve bookings with a single click

--- staff/urls.py
+++ staff/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import dashboard_page
+from .views import approve_booking
 
 urlpatterns = [
-    path("dashboard/", dashboard_page, name='dashboard')
+    path("dashboard/", dashboard_page, name='dashboard'),
+    path("approve/booking/<int:pk>/", approve_booking, name='approve'),
+
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from booking.models import Booking
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -19,3 +20,27 @@ def dashboard_page(request):
 
     return render(request, "staff/dashboard.html", context)
 
+
+def approve_booking(request, pk):
+    # booking = get_object_or_404(Booking, id)
+    booking = Booking.objects.get(id=pk)
+    
+    if request.method == "POST":
+        booking.status = "approved"
+        booking.save()
+        # Redirect back to the same page
+        return redirect('dashboard')  # Assuming 'dashboard' is the name of the view that renders the staff dashboard
+    
+    # If it's not a POST request, render the page as usual
+    bookings = Booking.objects.all()
+    approved = Booking.objects.filter(status='approved')
+    rejected = Booking.objects.filter(status='rejected')
+
+    context = {
+        'bookings': bookings,
+        'pendings': pendings,
+        'approved': approved,
+        'rejected': rejected,
+        'booking': booking,  # Include the booking object in the context
+    }
+    return render(request, "staff/dashboard.html", context)
