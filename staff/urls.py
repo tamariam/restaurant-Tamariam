59691a218e44dd4c73391cbb38eea43ded7327f4
sopiko abulidze <abulidze2019@gmail.com>
2024-02-27 15:35:23 +0000

    Create  reject_booking view to handle the rejection of bookings by staff members. Define corresponding URL patterns ,modify the dashboard.html template to include a form within table cells, allowing staff members to reject bookings directly from the dashboard interface.

--- staff/urls.py
+++ staff/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import dashboard_page
 from .views import approve_booking
+from .views import reject_booking
+
 
 urlpatterns = [
     path("dashboard/", dashboard_page, name='dashboard'),
     path("approve/booking/<int:pk>/", approve_booking, name='approve'),
+    path("reject/booking/<int:pk>/", reject_booking, name='reject'),
 
 ]
--- staff/views.py
+++ staff/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from booking.models import Booking
 from django.shortcuts import get_object_or_404
 
@@ -6,6 +6,11 @@ from django.shortcuts import get_object_or_404
 
 
 def dashboard_page(request):
+    ''' View function for rendering the dashboard page for staff members.
+    Retrieves all bookings from the database and categorizes them into pending,
+    approved, and rejected status. Then, passes these categorized bookings as
+    context variables to the dashboard template for rendering.'''
+
     bookings = Booking.objects.all()
     pendings = Booking.objects.filter(status='pending')
     approved = Booking.objects.filter(status='approved')
@@ -17,22 +22,20 @@ def dashboard_page(request):
         'approved': approved,
         'rejected': rejected,
     }
-
     return render(request, "staff/dashboard.html", context)
 
 
 def approve_booking(request, pk):
-    # booking = get_object_or_404(Booking, id)
-    booking = Booking.objects.get(id=pk)
-    
+    # booking = Booking.objects.get(id=pk)
+    booking = get_object_or_404(Booking, id=pk)
     if request.method == "POST":
         booking.status = "approved"
         booking.save()
         # Redirect back to the same page
-        return redirect('dashboard')  # Assuming 'dashboard' is the name of the view that renders the staff dashboard
-    
+        return redirect('dashboard')
     # If it's not a POST request, render the page as usual
     bookings = Booking.objects.all()
+    pendings = Booking.objects.filter(status='pending')
     approved = Booking.objects.filter(status='approved')
     rejected = Booking.objects.filter(status='rejected')
 
@@ -41,6 +44,28 @@ def approve_booking(request, pk):
         'pendings': pendings,
         'approved': approved,
         'rejected': rejected,
-        'booking': booking,  # Include the booking object in the context
+    }
+    return render(request, "staff/dashboard.html", context)
+
+
+def reject_booking(request, pk):
+    booking = get_object_or_404(Booking, id=pk)
+    if request.method == "POST":
+        booking.status = 'rejected'
+        booking.save()
+        # Redirect back to the same page
+        return redirect('dashboard')
+    # If it's not a POST request, render the page as usual
+    bookings = Booking.objects.all()
+    pendings = Booking.objects.filter(status='pending')
+    approved = Booking.objects.filter(status='approved')
+    rejected = Booking.objects.filter(status='rejected')
+
+    context = {
+        'bookings': bookings,
+        'pendings': pendings,
+        'approved': approved,
+        'rejected': rejected,
+        'booking': booking,
     }
     return render(request, "staff/dashboard.html", context)
